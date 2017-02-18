5b0c8ace80cb9708ac187f27ea96b9c9bc284cbe
Karola Marky <yonjuni@pinyto.de>
2017-02-18 20:27:58 +0100

    Finally able to display something

--- verification/views.py
+++ verification/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from verification.models import Candidate, Question
 
-# Create your views here.
+
+def test(request):
+    return render(request, 'institute_old.html', {})
+
+
+def institute_old_verifier(request):
+    candidate_code = request.candidate_code
+    vote = request.vote
+    ballotTracker = request.tracker
+    print(candidate_code)
+    candidate = Candidate.objects.get(pk=candidate_code)
+    question = Question.objects.all()[0]
+    return render(request, 'institute_old.html', {})
+
+
+def institute_one_verifier(request):
+    candidate_code = request.candidate_code
+    vote = request.vote
+    ballotTracker = request.tracker
+    print(candidate_code)
+    candidate = Candidate.objects.get(pk=candidate_code)
+    question = Question.objects.all()[0]
+    return render(request, 'institute_one.html', {})

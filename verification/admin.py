f1bd18751a2cf5f260e9cbbd2b986569512356b9
Karola Marky <yonjuni@pinyto.de>
2017-02-18 20:14:07 +0100

    DB stuff for admin

--- verification/admin.py
+++ verification/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from verification.models import Question
+from verification.models import Candidate
 
-# Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Candidate)
+

908db8eb478e2bd2aeecc21300b56882b155667d
Karola Marky <yonjuni@pinyto.de>
2017-02-21 19:46:05 +0100

    Refactoring

--- verification/views.py
+++ verification/views.py
@@ -2,23 +2,13 @@ from django.shortcuts import render
 from verification.models import Candidate, Question
 
 
-def test(request):
-    return render(request, 'verification/institute_old.html', {})
-
-
 def institute_old_verifier(request):
     return render(request, 'verification/institute_old.html', {})
-
-
-# Button that displays the proof in old version
-def verify_button():
-    pass
 
 
 def institute_one_verifier(request):
     candidate_code = request.candidate_code
     vote = request.vote
-    ballotTracker = request.tracker
     print(candidate_code)
     candidate = Candidate.objects.get(pk=candidate_code)
     question = Question.objects.all()[0]

78f977c29d771f0b8df092a9af06152b88f9ee0a
Karola Marky <yonjuni@pinyto.de>
2017-02-22 20:05:11 +0100

    Remains of DB removed

--- verification/admin.py
+++ verification/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from verification.models import Question
-from verification.models import Candidate
 
-
-admin.site.register(Question)
-admin.site.register(Candidate)
 

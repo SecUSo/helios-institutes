23ea2c326bdc5b51433fa65823093a54d1a63ec9
Karola Marky <yonjuni@pinyto.de>
2017-02-22 01:34:16 +0100

    Optimzing of DB by removing it

--- verification/views.py
+++ verification/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from verification.models import Candidate, Question
 
 
 def institute_old_verifier(request):
-    return render(request, 'verification/institute_old.html', {})
+    return render(request, 'verification/bsi_old.html', {})
 
 
 def institute_one_verifier(request):
-    candidate_code = request.candidate_code
-    vote = request.vote
-    print(candidate_code)
-    candidate = Candidate.objects.get(pk=candidate_code)
-    question = Question.objects.all()[0]
     return render(request, 'verification/institute_one.html', {})

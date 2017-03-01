baee94b68c0107ac4b8646c3543f9b40102f9d5f
Karola Marky <yonjuni@pinyto.de>
2017-03-01 14:47:20 +0100

    URLs to German templates

--- institutes/urls.py
+++ institutes/urls.py
@@ -4,9 +4,9 @@ from verification import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^verifier_one.*', views.institute_one_verifier, name='institute_one'),
-    url(r'^verifier_two.*', views.institute_two_verifier, name='institute_two'),
-    url(r'^verifier_osze.*', views.institute_osze_old, name='osze_old'),
-    url(r'^verifier.*', views.institute_old_verifier, name='bsi_old'),
-    url(r'.*', views.institute_one_verifier, name='institute_one'),
+    url(r'^verifier_one.*', views.institute_one_verifier, name='institute_oneDE'),
+    url(r'^verifier_two.*', views.institute_two_verifier, name='institute_twoDE'),
+    url(r'^verifier_osze.*', views.institute_osze_old, name='osze_oldDE'),
+    url(r'^verifier.*', views.institute_old_verifier, name='bsi_oldDE'),
+    url(r'.*', views.institute_one_verifier, name='institute_oneDE'),
 ]
